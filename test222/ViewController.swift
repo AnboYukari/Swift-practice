313d36cb3813e971a283d4da219609dc2eebff36
AnboYukari <b1018129@gmail.com>
2020-06-19 21:16:30 +0900

    はじめの一歩！

---
+++ test222/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test222
+//
+//  Created by 安保友香梨 on 2020/06/19.
+//  Copyright © 2020 anbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
