d931270d9e63c2fc86891df2492e4d45ec7f1d69
AnboYukari <b1018129@gmail.com>
2020-06-25 01:07:02 +0900

    リファクタリング前

---
+++ Mymusic/Mymusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mymusic
+//
+//  Created by 安保友香梨 on 2020/06/20.
+//  Copyright © 2020 anbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
